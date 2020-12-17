d1b8f44fdfe08138fef612032c67e0d0f68a6da9
fixonweb <contato@fixonweb.com.br>
2020-12-17 16:02:27 -0300

    0.1.1

--- fix-autologin.php
+++ fix-autologin.php
@@ -7,7 +7,7 @@
  * Author URI:      https://agencia.fixonweb.com.br
  * Text Domain:     fix-autologin
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * @package         Fix-Autologin
  */
